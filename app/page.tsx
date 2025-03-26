451073b91c86f06b0276321f2b55f777a721e0f2
eWeb0 <opdahlethan00@gmail.com>
2025-03-26 10:18:53 -0600

    homepage start

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className=' bg-black'>
-<div className="flex items-center justify-center">BingleFy</div>
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="flex gap-4">
-        <div className="w-20 h-20 bg-gray-500"></div>
-        <div className="w-20 h-20 bg-gray-500"></div>
-        <div className="w-20 h-20 bg-gray-500"></div>
+    <div className="bg-[url('/musMapbg1.jpg')] bg-center bg-contain bg-no-repeat min-h-screen">
+      <div className="flex items-center justify-center">BingleFy</div>
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="flex flex-col gap-4">
+          <div className="w-50 h-20 bg-gray-500 flex items-center justify-center">Home</div>
+          <div className="w-50 h-20 bg-gray-500 flex items-center justify-center">Login</div>
+          <div className="w-50 h-20 bg-gray-500 flex items-center justify-center">Search</div>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
